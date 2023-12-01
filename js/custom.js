49c543fc20557c4255a8d8814ceb499e4fac2792
Helen.Liu(劉巧涵) <92368114+MaruBigCAt@users.noreply.github.com>
2023-12-01 18:00:24 +0800

    1201

--- js/custom.js
+++ js/custom.js
@@ -4,6 +4,7 @@ let maxStep = $(".step").length - 1;
 
 let canScroll = true;
 const freezeScrollMs = 100;
+let touchStartY = 0;
 
 function _getNextStep(_newStep) {
   if (_newStep < minStep) return minStep;
@@ -11,9 +12,9 @@ function _getNextStep(_newStep) {
 
   return _newStep;
 }
+
 function _changeStep(_newStep) {
   const targetStep = _newStep * -1;
-
   $("#wrapper").css("transform", `translate3d(0, ${targetStep * 100}vh, 0)`);
 }
 
@@ -26,13 +27,14 @@ function handleGoToStepIndex(_newStep, _bifurcationClassName) {
 
   canScroll = false;
   $(".nextButton").attr("disabled", true);
+
   setTimeout(() => {
     canScroll = true;
     $(".nextButton").attr("disabled", false);
   }, freezeScrollMs);
 
   const newStep = _getNextStep(_newStep);
-  console.log("newStep",newStep)
+  console.log("newStep", newStep);
   currentStepIndex = newStep;
   _changeStep(newStep);
 }
@@ -50,17 +52,58 @@ function handleReStart() {
 }
 
 $(document).ready(() => {
-    maxStep = $(".step").length - 1;
-  $("#main").on("mousewheel", (event) => {
-    // prev step
+  maxStep = $(".step").length - 1;
+
+  function handleMouseWheel(event) {
+    // 上一步
     if (currentStepIndex < 3 && event.deltaY === 1) {
       handleGoToStepIndex(currentStepIndex - 1);
     }
 
     if (currentStepIndex > 1) return;
-    // next step
+
+    // 下一步
     if (event.deltaY === -1) {
       handleGoToStepIndex(currentStepIndex + 1);
+    }
+  }
+
+  function handleTouchMove(event) {
+    const deltaY = event.originalEvent.touches[0].clientY - touchStartY;
+
+    // 上一步
+    if (currentStepIndex < 3 && deltaY > 0) {
+      handleGoToStepIndex(currentStepIndex - 1);
+    }
+
+    if (currentStepIndex > 1) return;
+
+    // 下一步
+    if (deltaY < 0) {
+      handleGoToStepIndex(currentStepIndex + 1);
+    }
+  }
+
+  $("#main").on("mousewheel", function (event) {
+    if (window.innerWidth >= 768) {
+      handleMouseWheel(event);
+    }
+  });
+
+  $("#main").on("touchstart", (event) => {
+    touchStartY = event.originalEvent.touches[0].clientY;
+  });
+
+  $("#main").on("touchmove", function (event) {
+    if (window.innerWidth < 768) {
+      handleTouchMove(event);
+    }
+  });
+
+  // 保留原有的 mousewheel 行為
+  $(document).on("touchmove", function () {
+    if (window.innerWidth >= 768) {
+      $(document).trigger("mousewheel");
     }
   });
 });
